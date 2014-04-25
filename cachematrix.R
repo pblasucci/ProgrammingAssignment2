4330ecfba606a2a54668f86e9cb88196ad57fb72
Paulmichael Blasucci <pblasucci@gmail.com>
2014-04-25 10:43:04 -0500

    completed programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains functions for working with matrices
+## which can store the results of expensive operations.
 
-## Write a short comment describing this function
 
+## Creates a "wrapped" matrix which stores additional data.
+## Other functions in this file use this to cache certain calculation results.
 makeCacheMatrix <- function(x = matrix()) {
-
+    # privately-scoped variable which will hold the memoized value
+    i <- NULL 
+    
+    # updates input matrix
+    set <- function(y) {
+        x <<- y    # replace old matrix with new input
+        i <<- NULL # reset memozied value to "empty"
+    }
+    
+    # returns input matrix
+    get <- function() x
+    
+    # updates the memoized value
+    setcached <- function(y) i <<- y
+    
+    # returns the memoized value
+    getcached <- function()  i
+    
+    # expose getter,setter functions to outside world
+    list(get       = get, 
+         set       = set, 
+         getcached = getcached, 
+         setcached = setcached)
 }
 
 
-## Write a short comment describing this function
-
+## Computes the inverse of a "wrapped" square matrix, 
+## but only if the data (of the underlying matrix) changes    
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    # get memoized inverse from "wrapped" matrix
+    i <- x$getcached()
+    
+    if (!is.null(i)) {
+        # inverse is not "empty", so return it to caller
+        message("getting cached inverse")
+        return(i) # early exit... no further processing needed
+    } 
+    
+    # inverse was "empty", so...
+    # get "unwrapped" matrix
+    y <- x$get()
+    # calculate inverse
+    i <- solve(y, ...)
+    # store inverse in "wrapped" matrix
+    x$setcached(i)
+    # return inverse to caller
+    i
 }
